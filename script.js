ef17ff4f4e62fb6f93c942533ebbd17eb06fb2ea
farid neggaz <neggazfarid3@gmail.com>
2025-04-09 01:59:10 +0200

    j'ai cree 3 fichier de style et contact et script

---
+++ script.js
@@ -0,0 +1,9 @@
+document.querySelector("form").addEventListener("submit", function(event) {
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    
+    if (!name || !email) {
+        event.preventDefault();
+        alert("Tous les champs sont obligatoires !");
+    }
+});
